0c39efd74934382f1b481474332803a55f58b542
FerSoltez <19090483@alumno.utmetropolitana.edu.mx>
2025-01-23 14:35:19 -0600

    Primer Commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import sequelize from "./config/database";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middlewares
+app.use(express.json());
+
+// Routes
+
+// Database Connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
