852644a4fef941a5bc712a90680068ec81f0806c
Uziel Tzab <uzieltzab8@gmail.com>
2025-01-24 10:13:01 -0600

    Se añadió las Cors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import sequelize from "./config/database";
+import cors from "cors";
 
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Middlewares
+// Middlewares  
+app.use(cors());
 app.use(express.json());
 
 // Routes
