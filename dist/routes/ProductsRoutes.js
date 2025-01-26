7d89692c02d942fabe9b14c0845d5f2725c1ec30
FerSoltez <19090483@alumno.utmetropolitana.edu.mx>
2025-01-25 21:10:05 -0600

    Actualizacion 3 (Ya por favor)

--- dist/routes/ProductsRoutes.js
+++ dist/routes/ProductsRoutes.js
@@ -6,10 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const ProductsController_1 = __importDefault(require("../controllers/ProductsController"));
 const router = (0, express_1.Router)();
-router.post("products/", ProductsController_1.default.createProduct); // Crear un producto
-router.get("products/", ProductsController_1.default.getAllProducts); // Obtener todos los productos
-router.get("products/:id", ProductsController_1.default.getProduct); // Obtener un producto por ID
-router.put("products/:id", ProductsController_1.default.updateProduct); // Actualizar un producto completamente
-router.delete("products/:id", ProductsController_1.default.deleteProduct); // Eliminar un producto
-router.patch("products/:id", ProductsController_1.default.partialUpdateProduct); // Actualizar parcialmente un producto
+router.post("/products", ProductsController_1.default.createProduct); // Crear un producto
+router.get("/products", ProductsController_1.default.getAllProducts); // Obtener todos los productos
+router.get("/products/:id", ProductsController_1.default.getProduct); // Obtener un producto por ID
+router.put("/products/:id", ProductsController_1.default.updateProduct); // Actualizar un producto completamente
+router.delete("/products/:id", ProductsController_1.default.deleteProduct); // Eliminar un producto
+router.patch("/products/:id", ProductsController_1.default.partialUpdateProduct); // Actualizar parcialmente un producto
 exports.default = router;
--- src/routes/ProductsRoutes.ts
+++ src/routes/ProductsRoutes.ts
@@ -3,11 +3,11 @@ import productsController from '../controllers/ProductsController';
 
 const router = Router();
 
-router.post("products/", productsController.createProduct); // Crear un producto
-router.get("products/", productsController.getAllProducts); // Obtener todos los productos
-router.get("products/:id", productsController.getProduct); // Obtener un producto por ID
-router.put("products/:id", productsController.updateProduct); // Actualizar un producto completamente
-router.delete("products/:id", productsController.deleteProduct); // Eliminar un producto
-router.patch("products/:id", productsController.partialUpdateProduct as any); // Actualizar parcialmente un producto
+router.post("/products", productsController.createProduct); // Crear un producto
+router.get("/products", productsController.getAllProducts); // Obtener todos los productos
+router.get("/products/:id", productsController.getProduct); // Obtener un producto por ID
+router.put("/products/:id", productsController.updateProduct); // Actualizar un producto completamente
+router.delete("/products/:id", productsController.deleteProduct); // Eliminar un producto
+router.patch("/products/:id", productsController.partialUpdateProduct as any); // Actualizar parcialmente un producto
 
 export default router;
